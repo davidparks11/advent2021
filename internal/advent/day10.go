9da82eeab38623662854835521b73d6b7fa04e4b
David <davidparks11@gmail.com>
2021-12-10 23:03:27 -0500

    solve day 10 part 2

--- internal/advent/day10.go
+++ internal/advent/day10.go
@@ -1,6 +1,7 @@
 package advent
 
 import (
+	"sort"
 	"strconv"
 )
 
@@ -20,6 +21,7 @@ func (s *syntaxScoring) Solve() []string {
 	input := s.GetInputLines()
 	var results []string
 	results = append(results, strconv.Itoa(s.syntaxErrorScore(input)))
+	results = append(results, strconv.Itoa(s.part2(input)))
 	return results
 }
 
@@ -107,19 +109,116 @@ func (s *syntaxScoring) syntaxErrorScore(input []string) int {
 
 	score := 0
 	for _, line := range input {
-		stack := []rune{}
+		chunks := []rune{}
 		for _, char := range line {
 			if char == '(' || char == '[' || char == '{' || char == '<' {
-				stack = append(stack, char)
+				chunks = append(chunks, char)
 			} else {
-				if !matchingChunk(stack[len(stack)-1], char) {
+				if !matchingChunk(chunks[len(chunks)-1], char) {
 					score += syntaxScore(char)
 					break
 				}
-				stack = stack[:len(stack)-1]
+				chunks = chunks[:len(chunks)-1]
 			}
 		}
 	}
 
 	return score
+}
+
+/*
+Now, discard the corrupted lines. The remaining lines are incomplete.
+
+Incomplete lines don't have any incorrect characters - instead, they're missing some closing characters at the end of the line. To repair the navigation subsystem, you just need to figure out the sequence of closing characters that complete all open chunks in the line.
+
+You can only use closing characters (), ], }, or >), and you must add them in the correct order so that only legal pairs are formed and all chunks end up closed.
+
+In the example above, there are five incomplete lines:
+
+[({(<(())[]>[[{[]{<()<>> - Complete by adding }}]])})].
+[(()[<>])]({[<{<<[]>>( - Complete by adding )}>]}).
+(((({<>}<{<{<>}{[]{[]{} - Complete by adding }}>}>)))).
+{<[[]]>}<{[{[{[]{()[[[] - Complete by adding ]]}}]}]}>.
+<{([{{}}[<[[[<>{}]]]>[]] - Complete by adding ])}>.
+Did you know that autocomplete tools also have contests? It's true! The score is determined by considering the completion string character-by-character. Start with a total score of 0. Then, for each character, multiply the total score by 5 and then increase the total score by the point value given for the character in the following table:
+
+): 1 point.
+]: 2 points.
+}: 3 points.
+>: 4 points.
+So, the last completion string above - ])}> - would be scored as follows:
+
+Start with a total score of 0.
+Multiply the total score by 5 to get 0, then add the value of ] (2) to get a new total score of 2.
+Multiply the total score by 5 to get 10, then add the value of ) (1) to get a new total score of 11.
+Multiply the total score by 5 to get 55, then add the value of } (3) to get a new total score of 58.
+Multiply the total score by 5 to get 290, then add the value of > (4) to get a new total score of 294.
+The five lines' completion strings have total scores as follows:
+
+}}]])})] - 288957 total points.
+)}>]}) - 5566 total points.
+}}>}>)))) - 1480781 total points.
+]]}}]}]}> - 995444 total points.
+])}> - 294 total points.
+Autocomplete tools are an odd bunch: the winner is found by sorting all of the scores and then taking the middle score. (There will always be an odd number of scores to consider.) In this example, the middle score is 288957 because there are the same number of scores smaller and larger than it.
+
+Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score?
+*/
+func (s *syntaxScoring) part2(input []string) int {
+	syntaxScore := func(r rune) int {
+		switch r {
+		case '(':
+			return 1
+		case '[':
+			return 2
+		case '{':
+			return 3
+		case '<':
+			return 4
+		default:
+			return 0
+		}
+	}
+
+	matchingChunk := func(openChunk rune, closeChunk rune) bool {
+		switch openChunk {
+		case '(':
+			return closeChunk == ')'
+		case '[':
+			return closeChunk == ']'
+		case '{':
+			return closeChunk == '}'
+		case '<':
+			return closeChunk == '>'
+		default:
+			return false
+		}
+	}
+
+	var scores []int
+	for _, line := range input {
+		chunks := []rune{}
+		score := 0
+		for _, char := range line {
+			if char == '(' || char == '[' || char == '{' || char == '<' {
+				chunks = append(chunks, char)
+			} else {
+				if !matchingChunk(chunks[len(chunks)-1], char) {
+					chunks = nil //corrupted line - discard
+					break
+				}
+				chunks = chunks[:len(chunks)-1] //remove last element
+			}
+		}
+		if len(chunks) > 0 {
+			//loop through stack in reverse order to match chunk ending
+			for i := len(chunks) - 1; i >= 0; i-- {
+				score = score * 5 + syntaxScore(chunks[i]) 
+			}
+			scores = append(scores, score)
+		}
+	}
+	sort.Ints(scores)
+
+	return scores[len(scores)/2]
 }
